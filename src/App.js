eb1f6f63ffc64619be9ce145d4f998a63b163545
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-07-11 15:05:20 +0300

    created project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Courses from './Courses.js';
+import Loading from './Loading.js';
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  const deleteCourse = (id) => {
+    const afterDeletedCourses = courses.filter((course)=> course.id !== id); //idleri olanı yeni bi arreye atıyo (silinenleri yeniye atıyo)
+    setCourses(afterDeletedCourses)
+  }
+
+
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('http://localhost:3000/courses');
+      setCourses(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? ( 
+        <Loading /> 
+      ): (
+        <>
+          {courses.length === 0 ? (  //tüm elemanlar silinince fetchCoursesile zaten elemanları alıyoruz o yüzden tekrar component yapmıyoruz
+            <div className='refleshDiv'>
+              <h2 >Kursların hepsini sildin!</h2>
+              <button className='refleshDivBtn' onClick={()=>{fetchCourses()}}>Yenile</button>
+            </div>
+          ) : (<Courses courses={courses} removeCourse={deleteCourse} />) //data yüklenmemiş ama elaman var ise
+          }
+        </>
+      )}
     </div>
   );
 }
